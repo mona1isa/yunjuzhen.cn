bb053c6732f311b23c543d94c685a92712ecb2d9
ZhanyCloud <yu.zhang@yunjuzhen.cn>
2017-12-22 20:17:48 +0800

    main page update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-
+$(document).ready(function() {
+    $(".click-test").click(function() {
+        if($(".test").css("display")=="none"){
+            $(".test").show(1500);
+        }else {
+            $(".test").hide("slow");
+        }
+    });
+    $(".click-java").click(function() {
+        if($(".java").css("display")=="none"){
+            $(".java").show(1500);
+        }else {
+            $(".java").hide("slow");
+        }
+    });
+    $(".click-bigdata").click(function() {
+        if($(".bigdata").css("display")=="none"){
+            $(".bigdata").show(1500);
+        }else {
+            $(".bigdata").hide("slow");
+        }
+    });
+    $(".click-business").click(function() {
+        if($(".business").css("display")=="none"){
+            $(".business").show(1500);
+        }else {
+            $(".business").hide("slow");
+        }
+    });
+})
