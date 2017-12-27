c8ca93a4c55ab5d7593a9688d769336aa862813e
ZhanyCloud <yu.zhang@yunjuzhen.cn>
2017-12-27 10:49:21 +0800

    contact update

---
+++ js/contact.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $(window).resize(function () {
+        if (($("#header").width())>768) {
+            $('#header').addClass("navbar-index");
+            $('#header').removeClass("navbar-inverse");
+        } else {
+            $('#header').addClass("navbar-inverse");
+            $('#header').removeClass("navbar-index");
+        }
+    })
+
+    $(window).scroll(function() {
+        var scrollTop  = $(window).scrollTop()
+        if (scrollTop > 65) {
+            console.log(scrollTop);
+            $('#header').addClass("navbar-inverse");
+        } else {
+            $('#header').removeClass("navbar-inverse");
+        }
+    });
+})
--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+
+    $(window).resize(function () {
+        if (($("#header").width())>768) {
+            $('#header').addClass("navbar-index");
+            $('#header').removeClass("navbar-inverse");
+        } else {
+            $('#header').addClass("navbar-inverse");
+            $('#header').removeClass("navbar-index");
+        }
+    })
+
     $(".click-test").click(function() {
         if($(".test").css("display")=="none"){
             $(".test").show(1500);
@@ -27,9 +38,14 @@ $(document).ready(function() {
             $(".business").hide("slow");
         }
     });
-    $("body").scroll(function() {
-        alert($("#index-content").scrollTop());
-    })
-
+    $(window).scroll(function() {
+        var scrollTop  = $(window).scrollTop()
+        if (scrollTop > 65) {
+            console.log(scrollTop);
+            $('#header').addClass("navbar-inverse");
+        } else {
+            $('#header').removeClass("navbar-inverse");
+        }
+    });
 
 })
